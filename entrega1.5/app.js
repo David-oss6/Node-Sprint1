31730e095def85025b8ed52611cba3e31a4981e5
David <im.david85@gmail.com>
2023-01-14 20:42:11 +0100

    in proges

--- entrega1.5/app.js
+++ entrega1.5/app.js
@@ -1,20 +1,61 @@
+//Nivell 1 ex 1 y 2
+const zlib = require('zlib')
+const gzip = zlib.createGzip()
 const fs = require('fs')
+
 const nombreArchivo = 'archivo'
-const texto = 'loooooooooorem';
-const archivo = () => {
-    fs.mkdir(`./${nombreArchivo}`, (err) => {
+const texto = "Nivell 1 ex 2: Este es el texto creado";
+
+const crearTexto = () => {
+    fs.writeFile('./archivo/texto.txt', texto, (err) => {
         if (err) {
-            console.log('Algo salio mal en mkdir')
+            console.log('Algo salio mal en writeFile')
         } else {
-            console.log('Archivo creado')
-            fs.writeFile('./archivo/texto.txt', "Este es el texto", (err) => {
-                if (err) {
-                    console.log('Algo salio mal en writeFile')
-                } else { console.log('Texto creado') }
-            })
+            console.log('Nivell 1 ex 1: Archivo y texto creado')
+
         }
     })
 }
+const crearArchivo = () => {
+    fs.mkdir(`./${nombreArchivo}`, (err) => {
+        if (err) {
+            console.log('Algo salio mal en mkdir')
+        }
+    })
+}
+// crearArchivo()
+// crearTexto()
 
+const leerArchivo = () => {
+    setTimeout(() => {
+        console.log(fs.readFileSync("./archivo/texto.txt", "utf8"))
+    }, [2000])
 
-archivo()
+}
+// leerArchivo()
+
+// Nivell 1 exercici 3
+//Al ejecutar comprimir() desaparece la informacion de text.txt y aparece un mensaje de error en consola, no obstante comprime el archivo.
+
+// const input = fs.createWriteStream('./archivo/texto.txt')
+// const output = fs.createWriteStream('./archivo/texto.txt.gz')
+
+const comprimir = () => {
+    input.pipe(gzip).pipe(output)
+}
+// comprimir()
+
+//Nivell 2 ex 1
+const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const imprimirLista = () => {
+    for (i = 0; i < 10; i++) {
+        const z = new Promise((resolve) => {
+            setTimeout(() => {
+                console.log(lista[i])
+            }, [1000])
+        })
+        console.log(z)
+    }
+}
+imprimirLista()
