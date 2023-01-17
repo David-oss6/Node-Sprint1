bb42080ecc7defe220b244ad48cb253486a110ec
David Iglesias <im.david85@gmail.com>
2023-01-17 10:26:56 +0100

    1.5 lvl 3 incompleto

--- entrega1.5/app.js
+++ entrega1.5/app.js
@@ -34,15 +34,12 @@ const leerArchivo = () => {
 // leerArchivo()
 
 // Nivell 1 exercici 3
-//Al ejecutar comprimir() desaparece la informacion de text.txt y aparece un mensaje de error en consola, no obstante comprime el archivo.
-
-// const input = fs.createWriteStream('./archivo/texto.txt')
-// const output = fs.createWriteStream('./archivo/texto.txt.gz')
-
+// const input = fs.createWriteStream("./archivo/texto.txt");
+// const output = fs.createWriteStream("./archivo/texto.txt.gz");
 const comprimir = () => {
   input.pipe(gzip).pipe(output);
 };
-// comprimir()
+// comprimir();
 
 //Nivell 2 ex 1
 const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -75,26 +72,28 @@ const nivellDos = () => {
 
 //Paso 1 - Codificar archivo
 const input1 = fs.readdir("./archivo", (err, files) => {
-  files = files.toString();
-  console.log(files);
-  let codificado = Buffer.from(files).toString("base64");
-  console.log("Base64: ", codificado);
+  files = files.toString("base64");
+  console.log("Contenido de ./archivo-->", files);
   return files;
+  // files = Buffer.from(files).toString("base64");
+  // console.log("Base64: ", files);
+  // return files;
 });
 console.log(input1);
-// let codificado = Buffer.from(input1).toString("base64");
-// const output1 = fs.createWriteStream(`../${codificado}.hex`);
-// console.log("hexadecimal", codificado);
+// let codificadoHex = Buffer.from(input1).toString("base64");
+// console.log("codificadoHex", codificadoHex);
+// const output1 = fs.createWriteStream(`../entrega1.5/${input1}`);
 
-// let buff = new Buffer.from(input1);
-// let base64data = buff.toString("base64");
+// let codificadoHex = Buffer.from(input1).toString("hex");
+// const output2 = fs.createWriteStream(`../${codificadoHex}.hex`);
+// console.log("hexadecimal", codificadoHex);
 
 // console.log(
 //   '"' + archivoAcodificar + '" converted to Base64 is "' + base64data + '"'
 // );
 
 //Paso 2  - Encriptar con algoritmo
-let crypto = require("crypto");
+// let crypto = require("crypto");
 
 // let key = "14189dc35ae35e75ff31d7502e245cd9bc7803838fbfd5c773cdcd79b8a28bbd";
 // let cipher = crypto.createCipher("aes-256-cbc", key);
@@ -106,3 +105,6 @@ let crypto = require("crypto");
 // output.on("finish", function () {
 //   console.log("Encrypted file written to disk!");
 // });
+
+//Paso 3 Desencriptar
+// Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('ascii')
